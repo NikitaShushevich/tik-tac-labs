6b5880591bcf2b7ced3f9f1856b73209641fe73c
NikitaShushevich <nikita.shushevich@gmail.com>
2023-04-11 09:43:13 +0300

    added function description and documetization

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,12 +1,26 @@
-def print_board(b):
+def print_board(b:str)->str:
+    """
+The print_board (b) function displays the current state of the game board b.
+ It uses string formatting to display each board cell.
+    :param b:
+    :return:
+    """
     print("---------")
-    print("| {} {} {} |".format(b[1][1], b[1][2], b[1][3]))
-    print("| {} {} {} |".format(b[2][1], b[2][2], b[2][3]))
-    print("| {} {} {} |".format(b[3][1], b[3][2], b[3][3]))
+    print(f"| {b[1][1]} {b[1][2]} {b[1][3]} |")
+    print(f"| {b[2][1]} {b[2][2]} {b[2][3]} |")
+    print(f"| {b[3][1]} {b[3][2]} {b[3][3]} |")
     print("---------")
 
 
-def check_input(coordinate, b, count):
+def check_input(coordinate:int, b:str, count:int)->bool:
+    """
+The check_input function (coordinate, b, count) checks the correctness of the entered
+coordinates coordinate and whether this cell is free to move.
+    :param coordinate:
+    :param b:
+    :param count:
+    :return:
+    """
     if coordinate[0].isnumeric() != True or coordinate[1].isnumeric() != True:
         print('Можна вводити тільки цифри!')
         return False
@@ -25,7 +39,13 @@ def check_input(coordinate, b, count):
             return True
 
 
-def check_win(b):
+def check_win(b:str)->str:
+    """
+The check_win (b) function checks whether there is a winning state on the game board b.
+It checks rows, columns, and diagonals to make sure all cells have the same value (either "X" or "O").
+    :param b:
+    :return:
+    """
     columb_row = [any([b[1][n] == b[2][n] == b[3][n] == "X" or b[1][n] == b[2][n] == b[3][n] == "O" or b[n][1] == b[n][
         2] == b[n][3] == "X" or b[1][n] == b[2][n] == b[3][n] == "O"]) for n in range(1, 4)]
     cross = [b[1][1] == b[2][2] == b[3][3] == 'X' or b[1][1] == b[2][2] == b[3][3] == 'O' or b[1][3] == b[2][2] == b[3][
@@ -40,7 +60,12 @@ def check_win(b):
         return "продовження"
 
 
-def play_game():
+def play_game()->str:
+    """
+Function play_game () performs a game of cross-tic-tac-toe. It initializes an empty game board b and sets the move counter to 1.
+It displays the current state of the board until check_win (b) returns "finish" or "draw."
+    :return:
+    """
     b = [[], ["", " ", " ", " "], ["", " ", " ", " "], ["", " ", " ", " "]]
     count = 1
     start = "продовження"
